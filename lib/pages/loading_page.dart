608127727c4a10d7084228a75a9b36e531930495
networkside <mr.systemcore@gmail.com>
2023-09-09 16:07:09 +0330

    New Result Page

--- lib/pages/loading_page.dart
+++ lib/pages/loading_page.dart
@@ -4,9 +4,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 import 'package:http/http.dart';
 
+// ignore: must_be_immutable
 class LoadingPage extends StatefulWidget {
-  const LoadingPage({super.key});
-
+  LoadingPage({super.key, this.nextPage});
+  Widget? nextPage;
   @override
   State<LoadingPage> createState() => _LoadingPageState();
 }
@@ -14,10 +15,14 @@ class LoadingPage extends StatefulWidget {
 Response? response;
 
 class _LoadingPageState extends State<LoadingPage> {
+  Widget? nextPage;
   @override
   void initState() {
     super.initState();
-    getData('https://mtasa.com/api/', context);
+    widget.nextPage == null
+        ? nextPage = HomePage()
+        : nextPage = widget.nextPage;
+    getData('https://mtasa.com/api/', context, nextPage: nextPage);
   }
 
   @override
@@ -35,8 +40,17 @@ class _LoadingPageState extends State<LoadingPage> {
   }
 }
 
-void getData(String url, BuildContext context) async {
-  Uri uri = Uri.parse(url);
-  response = await get(uri);
-  changePage(HomePage(), context);
+void getData(String url, BuildContext context,
+    {Widget? nextPage, bool? doChangePage}) async {
+  try {
+    Uri uri = Uri.parse(url);
+    response = await get(uri);
+    nextPage == null ? nextPage = HomePage() : nextPage = nextPage;
+    doChangePage == null ? doChangePage = true : doChangePage = doChangePage;
+    if (doChangePage) {
+      changePage(nextPage, context);
+    }
+  } catch (e) {
+    print("Error. ${e}");
+  }
 }
--- lib/pages/result_page.dart
+++ lib/pages/result_page.dart
@@ -1,4 +1,8 @@
+import 'dart:convert';
+
+import 'package:api_app/functions/changePage.dart';
 import 'package:api_app/functions/showFields.dart';
+import 'package:api_app/pages/loading_page.dart';
 import 'package:flutter/material.dart';
 
 // ignore: must_be_immutable
@@ -25,14 +29,24 @@ class _ResultPageState extends State<ResultPage> {
     return Scaffold(
       appBar: getAppBar(),
       body: SafeArea(
-        child: getAppBody(),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Container(
+            height: MediaQuery.of(context).size.height,
+            child: getAppBody(context),
+          ),
+        ),
       ),
     );
   }
 
-  Widget getAppBody() {
+  Widget getAppBody(BuildContext context) {
     return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
       children: [
+        Image(
+          image: AssetImage('images/person2.png'),
+        ),
         Container(
           margin: EdgeInsets.all(5),
           decoration: BoxDecoration(
@@ -40,6 +54,21 @@ class _ResultPageState extends State<ResultPage> {
           ),
           child: showFields(theServer),
         ),
+        SizedBox(height: 20),
+        Padding(
+          padding: const EdgeInsets.fromLTRB(5, 0, 5, 0),
+          child: ElevatedButton(
+            onPressed: () {
+              refreshData(context);
+            },
+            child: Text("Refresh Details"),
+            style: ElevatedButton.styleFrom(
+              elevation: 3,
+              backgroundColor: Colors.red[800],
+              minimumSize: Size(double.infinity, 60),
+            ),
+          ),
+        )
       ],
     );
   }
@@ -51,5 +80,24 @@ class _ResultPageState extends State<ResultPage> {
       centerTitle: true,
       backgroundColor: Colors.deepPurple,
     );
+  }
+
+  void refreshData(BuildContext context) {
+    changePage(
+        LoadingPage(nextPage: ResultPage(theServer: theServer)), context);
+    getData('https://mtasa.com/api/', context, doChangePage: false);
+    for (var server in jsonDecode(response!.body)) {
+      if (server["ip"] == theServer.ip) {
+        print(theServer.ip);
+        setState(() {
+          theServer.name = server["name"];
+          theServer.maxplayers = server["maxplayers"].toString();
+          theServer.ip = server["ip"];
+          theServer.version = server["version"];
+          theServer.players = server["players"].toString();
+          theServer.port = server["port"].toString();
+        });
+      }
+    }
   }
 }
